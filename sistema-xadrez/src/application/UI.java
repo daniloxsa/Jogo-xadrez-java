c3b25f19f304ca8eb5b78c67948cdd8b5f1d641b
Danilo <128848973+daniloxsa@users.noreply.github.com>
2026-09-09 20:21:05 -0300

    limpar console pos ação + tratamento de erro na main

--- sistema-xadrez/src/application/UI.java
+++ sistema-xadrez/src/application/UI.java
@@ -7,39 +7,44 @@ import chess.ChessPiece;
 import chess.ChessPosition;
 
 public class UI {
-	
+
 	public static void printBoard(ChessPiece[][] pieces) {
-		for (int i=0; i<pieces.length; i++) {
+		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
-			for (int j=0; j<pieces.length; j++) {
+			for (int j = 0; j < pieces.length; j++) {
 				printPiece(pieces[i][j]);
-				
+
 			}
 			System.out.println();
 		}
 		System.out.println("  a b c d e f g h");
-			
+
 	}
-	
+
+	public static void clearScreen() {
+		for (int i = 0; i < 30; i++) {
+		    System.out.println();
+		}
+	}
+
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
-		String s = sc.nextLine();
-		char column = s.charAt(0);
-		int row = Integer.parseInt(s.substring(1));
-		return new ChessPosition(column, row);
-		}
-		catch (RuntimeException e) {
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
+		} catch (RuntimeException e) {
 			throw new InputMismatchException("Error reading ChessPosition");
 		}
 	}
-	
+
 	private static void printPiece(ChessPiece piece) {
 		if (piece == null) {
 			System.out.print("-");
 		} else {
 			System.out.print(piece);
 		}
-		
+
 		System.out.print(" ");
 	}
 
--- sistema-xadrez/src/boardgame/Piece.java
+++ sistema-xadrez/src/boardgame/Piece.java
@@ -1,8 +1,8 @@
 package boardgame;
 
 public class Piece {
-	protected Position position;
 	
+	protected Position position;
 	private Board board;
 
 	public Piece(Board board) {
