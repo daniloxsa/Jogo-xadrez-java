e41e338bc9dc9df2705ef4eebd85eb224c406242
Danilo <128848973+daniloxsa@users.noreply.github.com>
2026-09-09 21:21:22 -0300

    movimentação torre

--- sistema-xadrez/src/boardgame/Position.java
+++ sistema-xadrez/src/boardgame/Position.java
@@ -34,6 +34,12 @@ public class Position {
 	}
 	
 	
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	
 	@Override
 	public String toString() {
 		return row + ", " + column;
