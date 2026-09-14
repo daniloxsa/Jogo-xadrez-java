ec7c1b2d7b5103a6e39f4f5cacbcab73af571851
Danilo <128848973+daniloxsa@users.noreply.github.com>
2026-09-14 16:50:05 -0300

    Melhorias basicas para promoção de peças

--- sistema-xadrez/src/chess/ChessMatch.java
+++ sistema-xadrez/src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -120,7 +119,6 @@ public class ChessMatch {
 		
 		check = (testCheck(opponent(currentPlayer))) ? true : false;
 		
-		
 		if (testCheckMate(opponent(currentPlayer))) {
 			checkMate = true;
 		} else {
@@ -172,7 +170,7 @@ public class ChessMatch {
 		}
 		
 		if (!type.equals("B") && !type.equals("R") && !type.equals("N") && !type.equals("Q")) {
-			throw new InvalidParameterException("Invalid type for promotion");
+			return promoted;
 		}
 		
 		Position pos = promoted.getChessPosition().toPosition();
